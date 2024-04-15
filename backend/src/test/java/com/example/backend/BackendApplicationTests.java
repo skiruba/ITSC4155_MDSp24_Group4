43509d51bb192267c7e036e3314eea654730b326
pavukane <nguyenpham121998@gmail.com>
2024-04-15 16:11:40 -0400

    init backend hello world

--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package io.getarrays.server;
+package com.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServerApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
